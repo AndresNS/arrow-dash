edc6b0ad4bf892a8e4e06aa32ffe89100028f094
Andres Navarro S <andresm.navarro@gmail.com>
2024-05-26 13:16:56 -0400

    Refactor arrow animation to use an animation player instead of a script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 50.0
+const SPEED: float = 50.0
 
-@onready var arrow = $Arrow
 @onready var player_sprite = $PlayerSprite
+@onready var arrow_container = $ArrowContainer
+@onready var arrow_animation = $ArrowContainer/AnimationPlayer
+
 var direction = Vector2.UP
+var arrow_speed: float = 1.5
+
+func _ready():
+	arrow_animation.speed_scale = arrow_speed
 
 func _physics_process(delta):
-	var arrow_angle = arrow.arrow_angle
+	var arrow_angle = arrow_container.rotation
 	
 	if Input.is_action_just_pressed("ui_accept"):
-		player_sprite.rotation = arrow_angle + PI/2
-		direction = Vector2(cos(arrow_angle), sin(arrow_angle))
+		player_sprite.rotation = arrow_angle
+		direction = Vector2(cos(arrow_angle - PI/2), sin(arrow_angle - PI/2))
 	
 	position += SPEED * direction * delta
