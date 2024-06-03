0194f9bda760802606bb7ecaafcc1cc620cd2b65
Andres Navarro S <andresm.navarro@gmail.com>
2024-06-02 20:58:00 -0400

    Add static typing

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,18 +2,18 @@ extends CharacterBody2D
 
 const SPEED: float = 50.0
 
-@onready var player_sprite = $PlayerSprite
-@onready var arrow_container = $ArrowContainer
-@onready var arrow_animation = $ArrowContainer/AnimationPlayer
+@onready var player_sprite: Sprite2D = $PlayerSprite
+@onready var arrow_container: Node2D = $ArrowContainer
+@onready var arrow_animation: AnimationPlayer = $ArrowContainer/AnimationPlayer
 
-var direction = Vector2.UP
+var direction: Vector2 = Vector2.UP
 var arrow_speed: float = 1.5
 
-func _ready():
+func _ready() -> void:
 	arrow_animation.speed_scale = arrow_speed
 
-func _physics_process(delta):
-	var arrow_angle = arrow_container.rotation
+func _physics_process(delta: float) -> void:
+	var arrow_angle: float = arrow_container.rotation
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		player_sprite.rotation = arrow_angle
