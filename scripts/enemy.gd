37746d9459177765fbaa2e064aeeb8ea37f50a62
Andres Navarro S <andresm.navarro@gmail.com>
2024-07-01 23:37:04 -0400

    Update UI node and add score

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,13 +4,17 @@ class_name Enemy
 
 const SPEED: float = 65.0
 
+var game: Game = null
 var player: Player = null
-var attack_damage: int = 15
+var attack_damage: int = 25
 
 func _ready() -> void:
 	player = get_tree().current_scene.get_node("Player")
+	game = get_tree().root.get_node("Game")
 
 func _physics_process(_delta: float) -> void:
+	if (game.current_game_state != game.GameState.PLAYING):
+		return
 	var direction: Vector2 = (player.position - position).normalized()
 	velocity = direction * SPEED
 	move_and_slide()
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,7 +1,17 @@
 extends Node2D
 
+class_name Game
+
+enum GameState { MAIN_MENU, PLAYING, PAUSED, GAME_OVER }
+
 @onready var player: Player = $Player
-@onready var hp_label: Label = $Player/Camera2D/HPLabel
+@onready var hp_label: Label = $CanvasLayer/UI/HPLabel
+@onready var score_label: Label = $CanvasLayer/UI/ScoreLabel
+@onready var score_timer: Timer = $ScoreTimer
+
+var score: int = 0
+var score_per_second: int = 10
+var current_game_state: GameState = GameState.PLAYING
 
 func _ready() -> void:
 	update_hp_label(player.health, player.max_health)
@@ -9,5 +19,22 @@ func _ready() -> void:
 func update_hp_label(health: int, max_health: int) -> void:
 	hp_label.text = "HP: %d/%d" % [health, max_health]
 
+func update_score(amount: int) -> void:
+	if(current_game_state != GameState.PLAYING):
+		return
+	score += amount
+	score_label.text = "Score: %d" % score
+
+func game_over() -> void:
+	current_game_state = GameState.GAME_OVER
+	player.queue_free()
+	print("Game Over")
+
 func _on_player_health_changed(new_health: int) -> void:
 	update_hp_label(new_health, player.max_health)
+	if (new_health <= 0):
+		game_over()
+
+func _on_score_timer_timeout() -> void:
+	update_score(score_per_second)
+	score_timer.start()
